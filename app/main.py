501db59c143b555060834e4ad28d37faea3cf6bc
michalJuniorabramczyk <michaljuniorabramczyk@gmail.com>
2025-08-17 10:48:37 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,21 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.strip().split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    _, source, target = parts
+
+    if source == target:
+        return
+
+    if not os.path.exists(source):
+        return
+
+    with open(source, "r", encoding="utf-8") as file_in, open(
+        target, "w", encoding="utf-8"
+    ) as file_out:
+        file_out.write(file_in.read())
